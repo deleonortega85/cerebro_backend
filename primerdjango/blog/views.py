3edc3a109e34451d094789f50f062d02ebb5195c
LEÓN <Dlo85>
2025-06-30 12:30:50 -0500

    todo hecho con amor a Django

--- primerdjango/blog/views.py
+++ primerdjango/blog/views.py
@@ -2,22 +2,19 @@
 from django.shortcuts import render
 
 def inicio(request):
-    return render(request, 'blog/inicio.html')
-
-def sobre_mi(request):
-    return render(request, 'blog/sobreMi.html')
-
-def publicar(request):
     # publicaciones = [
     #     "Post 1: Django es increíble",
     #     "Post 2: Qué es MVT",
     #     "Post 3: Cómo crear una app"
     # ]
     publicacion = [
-         {"titulo": "1", "contenido": "Django es Increible."},
-         {"titulo": "2", "contenido": "Que es MVT"},
-         {"titulo": "3", "contenido":"¿Cómo Crear esta app?"}
+         {"titulo": "Post 1", "contenido": "Django es Increible"},
+         {"titulo": "Post 2", "contenido": "Que es MVT"},
+         {"titulo": "Post 3", "contenido":"¿Cómo Crear esta app?"}
     ]
     context = {"publicacion": publicacion}
 
-    return render(request, 'blog/inicio.html', {'posts': context})
+    return render(request, 'blog/inicio.html', context)
+
+def sobre_mi(request):
+    return render(request, 'blog/sobreMi.html')
