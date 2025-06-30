97831781fbaf948e0f6dfdc564d7d3778af2be7b
LEÓN <Dlo85>
2025-06-30 10:53:53 -0500

    subir inicioi.html

--- primerdjango/blog/urls.py
+++ primerdjango/blog/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'
-
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('sobreMi/', views.sobre_mi, name='sobreMi'),
