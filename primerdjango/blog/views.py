10e91d4cda342d6dd96680efcd4276dca9fc0565
LEÓN <Dlo85>
2025-06-30 11:34:21 -0500

    update

--- primerdjango/blog/views.py
+++ primerdjango/blog/views.py
@@ -6,3 +6,18 @@ def inicio(request):
 
 def sobre_mi(request):
     return render(request, 'blog/sobreMi.html')
+
+def publicar(request):
+    # publicaciones = [
+    #     "Post 1: Django es increíble",
+    #     "Post 2: Qué es MVT",
+    #     "Post 3: Cómo crear una app"
+    # ]
+    pub = [
+        {"1": "Django es Increible.",
+         "2": "Que es MVT",
+         "3": "¿Cómo Crear esta app?"
+         }
+    ]
+
+    return render(request, 'blog/inicio.html', {'posts': pub})
