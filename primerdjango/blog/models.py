4f28911a5a30597c44d422133f7fb3a649e9e934
LEÓN <Dlo85>
2025-07-01 09:46:40 -0500

    modelos

--- primerdjango/blog/models.py
+++ primerdjango/blog/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 # Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    autor = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    publicado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo + " " + self.autor
