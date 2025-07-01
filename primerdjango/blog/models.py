c7e91453ad4dd91654981548999528278d0cac87
LEÓN <Dlo85>
2025-07-01 10:30:43 -0500

    modelos

--- primerdjango/blog/models.py
+++ primerdjango/blog/models.py
@@ -9,3 +9,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class Comentario(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comentarios')
+    autor = models.CharField(max_length=100)
+    texto = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentario de {self.autor}"
