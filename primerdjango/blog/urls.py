7da3f1d8d295438ff179343a4d5fc010cd3cff39
LEÓN <Dlo85>
2025-07-03 16:48:25 -0500

    upgrade listas_posts.html

--- primerdjango/blog/urls.py
+++ primerdjango/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('sobreMi/', views.sobre_mi, name='sobreMi'),
     path('blog', views.lista_posts, name='lista_posts'),
+    path('post/<int:id>/', views.detalle_post, name='detalle_post'),
 ]
